6722a7d4eff5ae9d057e41d53fc028a18e110973
Talip <talipakcelik@hacettepe.edu.tr>
2022-01-27 14:54:35 +0300

    starter files and webpack configuration for project

---
+++ index.308eac068dfc75af280a.js
@@ -0,0 +1,20 @@
+(self["webpackChunktodo_list"] = self["webpackChunktodo_list"] || []).push([["index"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+console.log("qeqwe");
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguMzA4ZWFjMDY4ZGZjNzVhZjI4MGEuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7QUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL3RvZG8tbGlzdC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcInFlcXdlXCIpO1xyXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
