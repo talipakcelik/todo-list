0185f537b43fa80956b5bc4a6706da775a3924f5
Talip <talipakcelik@hacettepe.edu.tr>
2022-01-28 22:46:19 +0300

    each project's name is rendered screen via change and click event listener

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {
   todoStore,
   renderToScreen,
   loopTodoStore,
+  projectCreator,
 } from "./todo.js";
 import style from "./style.css";
 
@@ -15,6 +16,7 @@ const section = document.querySelector("section");
 const main = document.querySelector("main");
 const menu = document.querySelector("menu");
 const projectContainer = document.querySelector(".project-container");
+
 // const deleteButton = document.querySelector(".delete");
 
 buttonInput.addEventListener("click", function () {
@@ -24,39 +26,59 @@ buttonInput.addEventListener("click", function () {
   console.log(todoStore);
 });
 
-section.addEventListener("click", function (e) {
-  if (e.target.textContent === "Projects") {
-    taskList.remove();
-    const projectsHeader = document.createElement("h1");
-    projectsHeader.textContent = "Project";
-    main.append(projectsHeader);
-  }
-  if (e.target.classList.contains("add-project")) {
+menu.addEventListener("click", function (e) {
+  if (e.target.textContent === "New") {
+    projectCreator();
+    // const newProject = document.createElement("input");
+    // newProject.placeholder = "Untitled";
+    // newProject.classList.add("new-project");
+    // const projectAdd = document.createElement("span");
+    // projectAdd.textContent = "+";
+    // projectAdd.classList.add("add-project");
+
+    // projectContainer.append(newProject);
+    // projectContainer.append(projectAdd);
+    /////
     if (!document.querySelector(".project-title")) {
-      taskList.remove();
+      // taskList.remove();
+      if (document.querySelector(".task-container")) {
+        document.querySelector(".task-container").remove();
+      }
+
       const projectTitle = document.createElement("h2");
       projectTitle.classList.add("project-title");
-      main.append(projectTitle);
-      projectTitle.textContent = e.target.previousSibling.value;
+      main.prepend(projectTitle);
+      projectTitle.textContent = "Untitled";
+      document
+        .querySelector(".new-project")
+        .addEventListener("change", function (e) {
+          projectTitle.textContent = e.target.value;
+        });
     }
   }
 });
-
-menu.addEventListener("click", function (e) {
-  if ((e.target.textContent = "New")) {
-    const newProject = document.createElement("input");
-    newProject.placeholder = "Untitled";
-    newProject.classList.add("new-project");
-    const projectAdd = document.createElement("span");
-    projectAdd.textContent = "+";
-    projectAdd.classList.add("add-project");
-
-    projectContainer.append(newProject);
-    projectContainer.append(projectAdd);
-
-    taskList.remove();
-    const projectTitle = document.createElement("h2");
+section.addEventListener("click", function (e) {
+  if (e.target.textContent === "Projects") {
+    //   taskList.remove();
+    //   const projectsHeader = document.createElement("h1");
+    //   projectsHeader.textContent = "Project";
+    //   main.append(projectsHeader);
+  }
+  if (e.target.classList.contains("add-project")) {
+    if (!document.querySelector(".project-title")) {
+      // taskList.remove();
+      // const projectTitle = document.createElement("h2");
+      // projectTitle.classList.add("project-title");
+      // main.append(projectTitle);
+      // projectTitle.textContent = e.target.previousSibling.value;
+    }
+  }
+  if (e.target.placeholder === "Untitled") {
+    //   // if (!document.querySelector(".project-title").textContent === "Untitled") {
+    document.querySelector(".project-title").textContent =
+      e.target.value || "Untitled";
+    //   // }
   }
 });
 
-export { taskList, titleInput };
+export { taskList, titleInput, projectContainer };
