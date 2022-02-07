57df9fcf1a3de1b68a773817ce58300c851ed2ae
Talip <talipakcelik@hacettepe.edu.tr>
2022-02-07 12:51:11 +0300

    Fix favicon

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
       title: "todo-list",
       filename: "index.html",
       template: path.resolve(__dirname, "src/temp.html"),
-      favicon: "checkmark-done-outline.svg",
+      favicon: "checkmark.svg",
     }),
   ],
 };
